c0e779f9b01511b97ffb97fc9f39d63da62b7678
Gabriel Costa <gabrielszcosta@gmail.com>
2019-08-07 19:49:32 -0400

    Ajuste no server.js

--- src/server.js
+++ src/server.js
@@ -6,12 +6,6 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
-// mongoose.connect(
-//   "mongodb+srv://omnistack:omnistack@cluster0-l6do1.mongodb.net/omnistack8?retryWrites=true&w=majority",
-//   {
-//     useNewUrlParser: true
-//   }
-// );
 
 mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true
