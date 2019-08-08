d6259fa0bcf6c8c570ca0294810d7ec1a5f12d35
Gabriel Costa <gabrielszcosta@gmail.com>
2019-08-08 00:22:24 -0400

    Ajuste no metodo de listagem de devs

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -4,7 +4,7 @@ const Dev = require('../models/Dev');
 module.exports = {
 	async index(req, res) {
 		const { user } = req.headers;
-		const loggedDev = await Dev.find();
+		const loggedDev = await Dev.findById(user);
 
 		const users = await Dev.find({
 			$and: [{ _id: { $ne: user } }, { _id: { $nin: loggedDev.likes } }, { _id: { $nin: loggedDev.dislikes } }]
